3e5b8e1561aa41823e6b1b27d7fdd2ece0838482
Muhammad Qinthar <qintharalmaliki@gmail.com>
2025-07-17 20:29:14 +0700

    update base on vite config

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react/', // <--- ini WAJIB sesuai nama repo
+  base: '/manish/', // <--- ini WAJIB sesuai nama repo
 })
